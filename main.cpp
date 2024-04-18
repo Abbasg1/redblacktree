0ed525cc58355a4bf2e440ec3ba163bb719f54b6
“412140” <412140@bsd48.org>
2024-04-18 05:56:00 +0100

    node class

--- main.cpp
+++ main.cpp
@@ -1,2 +1,47 @@
 #include <iostream>
-#include <>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+
+  Node* right;
+  Node* left;
+  Node* parent;
+
+  bool color; //true is red black is false
+  //constructor called
+  Node(int data, Node* right, Node* left, Node* parent, bool color)
+  {
+    this->data = data;
+    this->right = right;
+    this->left = left;
+    this->parent = parent;
+    this->color = color;
+  }
+
+  //default
+  Node()
+  {
+    data = 0;
+    right = nullptr;
+    left = nullptr;
+    parent = nullptr;
+    color = false;
+  }
+
+  ~Node(){}//nothing to deallocate 
+
+};
+
+
+
+int main()
+{
+
+
+}
